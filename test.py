9398949f8dd937136d32cf1bf1ad1833adcff3ee
TrungLoze <nguyenhoangtrung26092006@gmail.com>
2023-06-02 08:55:03 +0700

    tim_1

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ time.sleep(1)
 pos = pad.position()
 
 # Auto click
-for i in range(10):
-    pad.click(pos)
-    pad.write('Hello Ken Mom')
-    pad.press('enter')
+pad.click(pos)
+pad.write('Hello Ken Mom')
+pad.press('enter')
+
+# Tìm theo đối tượng
