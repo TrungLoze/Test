15a1533035fd90d0b7f991dab381ac6a9de9524e
TrungLoze <nguyenhoangtrung26092006@gmail.com>
2023-06-02 09:17:22 +0700

    Tim_test_1

--- test.py
+++ test.py
@@ -2,11 +2,25 @@ import pyautogui as pad
 import time
 # Tìm tọa độ
 time.sleep(1)
-pos = pad.position()
+# pos = pad.position()
 
 # Auto click
-pad.click(pos)
-pad.write('Hello Ken Mom')
-pad.press('enter')
+# pad.click(pos)
+# pad.write('Hello Ken Mom')
+# pad.press('enter')
 
 # Tìm theo đối tượng
+hong=''
+vang=''
+while True:
+    loc_hong=pad.locateOnScreen(hong)
+    loc_vang=pad.locateOnScreen(vang)
+    if loc_hong > loc_vang:
+        pad.press('right')
+    elif loc_hong < loc_vang:
+        pad.press('left')
+
+
+
+
+
