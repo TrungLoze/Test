1bc2202323c16a7084645e6d2f803c1026546e5d
TrungLoze <nguyenhoangtrung26092006@gmail.com>
2023-06-02 08:44:46 +0700

    add left right

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import pyautogui as pad
+import time
 # Tìm tọa độ
+time.sleep(1)
 pos = pad.position()
-print(pos)
 
 # Auto click
 pad.click(pos)
+pad.press('left')
+pad.press('left')
+pad.press('left')
+pad.press('left')
+pad.press('left')
+pad.press('right')
+pad.press('right')
+pad.press('right')
